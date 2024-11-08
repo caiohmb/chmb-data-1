91494d8f1d562e41a772cf4b0c7cf5d29fc25554
caiohmb <caioh.maiab@gmail.com>
2024-11-07 22:11:40 -0300

    rename database

--- app/job/glue-etl.py
+++ app/job/glue-etl.py
@@ -1,16 +1,12 @@
 from pyspark.sql import SparkSession
 
-# Criação da Spark session com as configurações do Glue Catalog e Iceberg
+# Criação da Spark session
 spark = SparkSession.builder \
     .appName("Glue Job with Iceberg") \
-    .config("spark.sql.catalog.glue_catalog", "org.apache.iceberg.spark.SparkCatalog") \
-    .config("spark.sql.catalog.glue_catalog.warehouse", "s3://dev-us-east-1-data-1-project/bronze/iceberg/") \
-    .config("spark.sql.catalog.glue_catalog.catalog-impl", "org.apache.iceberg.aws.glue.GlueCatalog") \
-    .config("spark.sql.catalog.glue_catalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO") \
-    .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
     .getOrCreate()
 
-dataFrame = spark.sql("SELECT * FROM glue_catalog.var.glue_database_name.data")
+# Consultando os dados do Glue Catalog
+dataFrame = spark.sql("SELECT * FROM glue_catalog.dev-database-data-1-project.data")
 
 # Registrar o DataFrame como uma tabela temporária
 dataFrame.createOrReplaceTempView("tmp_table")
